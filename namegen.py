7433352cce8b0f90fcd31fc0fac3d470467743ed
Leland Rolofson <git@lrolofson.com>
2023-10-19 07:54:28 -0700

    add permutation calculation printout

--- namegen.py
+++ namegen.py
@@ -8,5 +8,9 @@ namesFirst = listify("first-names.txt")
 namesLast = listify("last-names.txt")
 namesSuffix = listify("last-name-suffixes.txt")
 
+permuationCount = (len(namesFirst) * len(namesLast) * len(namesSuffix))
+
+print("Currently there are " + str(len(namesFirst)) + " * " + str(len(namesLast)) + " * " + str(len(namesSuffix)) + " = " + str(permuationCount) + " possible combinations.")
+
 for i in range(10):
     print(random.choice(namesFirst) + " " + random.choice(namesLast) + random.choice(namesSuffix))
